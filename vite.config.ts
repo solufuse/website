9c8bf46221deb9f8c622918760301587ea5af33d
solufuse <solufuse@gmail.com>
2026-02-10 09:46:26 +0000

    fix update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: false, filename: 'dist/stats.html' }) // Add the visualizer plugin
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor_react';
+            }
+            if (id.includes('firebase')) {
+              return 'vendor_firebase';
+            }
+            if (id.includes('react-markdown') || id.includes('remark') || id.includes('rehype') || id.includes('katex') || id.includes('highlight.js')) {
+              return 'vendor_markdown';
+            }
+            return 'vendor';
+          }
+        },
+      },
     },
   },
 })
